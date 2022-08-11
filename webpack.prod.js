147dc23a5cc9977b3114d79805b6a8402cd7c67a
Camilo Arango <jmilo67@hotmail.com>
2022-08-11 00:11:38 -0500

    minimize code and production webpack config added

---
+++ webpack.prod.js
@@ -0,0 +1,64 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const cssMinimizer = require('css-minimizer-webpack-plugin');
+const terser = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+
+  output: {
+    clean: true,
+    filename:'main.[contenthash].js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        options: {
+          sources: false,
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /styles.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimize:true,
+    minimizer: [
+        new cssMinimizer (),
+        new terser ()
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Prueba de webpack",
+      //filename: cambiar el nombre del archivo,
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[fullhash].css",
+      ignoreOrder: false,
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets/", to: "assets/" }],
+    }),
+  ],
+};
